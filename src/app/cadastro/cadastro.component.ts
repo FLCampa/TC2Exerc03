ea302cf6024df4dd58ee99db2377bf98afdaa1fc
FLCampa <flcampanelli@gmail.com>
2021-09-24 09:10:25 -0300

    commit exercicio 3

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -19,17 +19,14 @@ export class CadastroComponent implements OnInit {
   ngOnInit(): void {}
 
   cadastrar(aluno: Aluno): void {
-    if (aluno.idade < 18) {
-      alert('Aluno não cadastrado, idade mínima de 18 anos!')
-    }
-    if (aluno.nome.length === 0 || !aluno.nome.trim()) {
-      alert('Aluno não cadastrado, nome vazio!')
+    if (aluno.idade < 18 || aluno.nome.length === 0 || !aluno.nome.trim()) {
+      alert('Erro, houve algum erro no cadastro!')
+      return
     }
     if (aluno.foto === '') {
       aluno.foto = 'http://www.gravatar.com/avatar/?d=mp'
-    } else {
-      listaAlunosCadastrados.push(aluno)
-      alert('Aluno ' + this.aluno.nome + ' cadastrado com sucesso!')
     }
+    listaAlunosCadastrados.push(aluno)
+    alert('Sucesso, Aluno ' + this.aluno.nome + ' cadastrado!')
   }
 }
